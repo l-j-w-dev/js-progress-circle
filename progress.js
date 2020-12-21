34235dc59f3f232a87d588de5c6cb4bb4dfd0e94
jwon <jwon302@gmail.com>
2020-12-21 11:26:59 +0900

    .

---
+++ progress.js
@@ -0,0 +1,58 @@
+drawCircle(document.querySelector('.circle'), 10, 30, 1.5, '#f44', '#44f');
+		drawCircle(document.querySelector('.circle2'), 20, 60, 0.5, '#f4f', '#4f4');
+		drawCircle(document.querySelector('.circle3'), 10, 20, 0.3, '#f55', '#55f');
+		drawCircle(document.querySelector('.circle4'), 8, 83, 1, '#f55', '#55f');
+
+
+		function drawCircle(parent, thick, percent, tick, color1, color2) { // 부모요소, 두께, 퍼센트, 채워지는 속도, 꽉찼을때 color, 시작 color
+			const pt = percent;
+			const radius = parent.clientHeight / 2;
+			if (percent >= 100) {
+				percent = 99.99;
+			}
+			percent *= 2;
+			const canvas = document.createElement('canvas');
+			canvas.height = radius * 2;
+			canvas.width = radius * 2;
+			parent.appendChild(canvas);
+			const ctx = canvas.getContext('2d');
+			ctx.scale(1, 1);
+
+			let div = document.createElement('div');
+			div.className = 'percent';
+			parent.appendChild(div);
+			let size = 0;
+			let interval = setInterval(function () {
+				if (size + tick >= percent) {
+					size = percent;
+				}
+				ctx.clearRect(0, 0, radius * 2, radius * 2)
+				ctx.save();
+				ctx.beginPath();
+				ctx.fillStyle = '#abc';
+				ctx.arc(radius, radius, radius, 0, Math.PI * 2);
+				ctx.fill();
+
+				ctx.beginPath();
+				ctx.fillStyle = '#333333';
+				ctx.arc(radius, radius, radius - thick, 0, Math.PI * 2);
+				ctx.fill();
+
+				div.innerHTML = Math.round(size / 2) + '%';
+				div.style.fontSize = radius / 3 + "px";
+				ctx.beginPath();
+				ctx.lineWidth = thick;
+				const gr = ctx.createLinearGradient(0, 0, (2 * radius * Math.PI) * size / 100 / 2, 0);
+				gr.addColorStop("0", color1);
+				gr.addColorStop("1", color2);
+				ctx.strokeStyle = gr;
+				ctx.arc(radius, radius, radius - thick / 2, Math.PI * 2 - Math.PI / 2, (Math.PI * size / 100) - (Math
+					.PI / 2));
+				ctx.stroke();
+				size += tick;
+				if (size >= percent) {
+					clearInterval(interval);
+				}
+			}, 10)
+			ctx.restore();
+		}
